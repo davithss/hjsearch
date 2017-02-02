c1f72baa7ff1d2c3710285f7ab8419c6dd83eaa5
Davi Thiesse <davithss@gmail.com>
2017-02-02 19:26:59 -0300

    Application and Routes settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :searches, only: [:index, :new, :create] do
+    collection do
+      get :autocomplete
+    end
+  end
+  root 'searches#index'
+  mount Searchjoy::Engine, at: "searchjoy"
 end
