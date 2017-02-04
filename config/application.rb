1356f0bf513d465a01604b0a2371247ee78b25a4
Davi Thiesse <davithss@gmail.com>
2017-02-04 01:44:59 -0300

    Assets precompiling

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,5 @@ Bundler.require(*Rails.groups)
 
 module Hjsearch
   class Application < Rails::Application
-    config.serve_static_assets = true
-    config.assets.initialize_on_precompile = false
   end
 end
